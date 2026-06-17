aa209ec84dab550e163e920e7269f0aeeb8ed76a
mujeebmasi <shaikhabdulmujeeb0415@gmail.com>
2026-06-17 20:15:34 +0530

    Basic CRUD operations

---
+++ backend/app/db/crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+
+from app.db.models import User
+
+
+def get_user_by_email(
+    db: Session,
+    email: str
+):
+    return (
+        db.query(User)
+        .filter(User.email == email)
+        .first()
+    )
+
+
+def create_user(
+    db: Session,
+    email: str
+):
+    user = User(
+        email=email,
+        is_verified=True
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
